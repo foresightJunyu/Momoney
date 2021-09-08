63486144b6c6d683377931dcd8a7beb3b3fdf1df
foresightJunyu <373034891@qq.com>
2021-09-08 15:43:39 +0800

    重复3次页面布局

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,15 +1,30 @@
 <template>
-<div>Statistics
-  <Nav/>
-</div>
+  <div class="nav-wrapper">
+    <div class="content">
+      Statistics
+    </div>
+    <Nav/>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
-name: "Statistics",
+  name: "Statistics",
 }
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  border: 1px solid blue;
+  background: rgb(79, 60, 136);
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
