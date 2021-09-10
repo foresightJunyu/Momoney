a8745b6b88728937285cac19635fcc3792d13da4
foresightJunyu <373034891@qq.com>
2021-09-10 21:57:54 +0800

    添加全局样式和全局变量

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,29 @@
 <template>
-<!--  <div id="app">-->
-<!--    <div id="nav">-->
-<!--      <router-link to="/">Home</router-link> |-->
-<!--      <router-link to="/about">About</router-link>-->
-<!--    </div>-->
-<!--    <router-view/>-->
-<!--  </div>-->
+  <!--  <div id="app">-->
+  <!--    <div id="nav">-->
+  <!--      <router-link to="/">Home</router-link> |-->
+  <!--      <router-link to="/about">About</router-link>-->
+  <!--    </div>-->
+  <!--    <router-view/>-->
+  <!--  </div>-->
 
-  <div>
+  <div id="app">
     <!--相当于容器container，让component能定位到这里来显示-->
-    <router-view />
-    </div>
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-body{
-  line-height: 1.5;
-}
-a{
-  text-decoration: none;
-  color: inherit;
-}
-// 这个a{}可以把下划线去掉
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import "~@/assets/style/reset.scss";
+@import "~@/assets/style/helper.scss";
+
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #333;
+  line-height: 1.5;
+  font-family: $font-kai;
 }
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
