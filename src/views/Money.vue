e71a64cc34424817c3aa66756ca7a806f05b927d
foresightJunyu <373034891@qq.com>
2021-09-15 16:45:41 +0800

    在 localstorage 里，把 record 的复制保存至 list

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-      <NumberPad @update:value="onUpdateAmount"/>
+      <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
       <Types :value.sync="record.type"/>
       <Notes @update:value="onUpdateNotes"/>
       <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
-      {{record}}
+      {{ record }}
     </Layout>
   </div>
 </template>
@@ -16,7 +16,7 @@ import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Notes from "@/components/Money/Notes.vue";
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
 
 type Record = {
   tags: string[]
@@ -31,6 +31,7 @@ type Record = {
 
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
+  recordList: Record[] = [];
   record: Record = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -45,6 +46,20 @@ export default class Money extends Vue {
 
   onUpdateAmount(value: string) {
     this.record.amount = parseFloat(value);
+  }
+
+  saveRecord() {
+    const deepClone = JSON.parse(JSON.stringify(this.record))
+    this.recordList.push(deepClone);
+    // 上面这出了一个bug，这样写 this.recordList.push(this.record);
+    // this.record 只是一个引用，而没有复制出一个新的，所以提交多少次都是一样的数据在localstorage里面
+    console.log(deepClone)
+  }
+
+  @Watch('recordList')
+  onRecordListChange() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+
   }
 }
 </script>
